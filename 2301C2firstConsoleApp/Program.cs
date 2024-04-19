60b57186d97635e142b7a5dd85863cc636aca274
harisnaseer24 <harisnaseer258@gmail.com>
2024-04-19 12:43:11 +0500

    C# Class work

---
+++ 2301C2firstConsoleApp/Program.cs
@@ -0,0 +1,182 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, Haris!");
+
+//DATATYPES
+/*
+ 
+int age = 18;
+long daysLived = 14636213256L;
+float percentage = 78.99F;
+double speedOflight = 3.8374847374894767;
+decimal points = 4.993M;
+
+
+
+//char favCharacter = '\n';
+//string name = "Haris Nasser";
+// bool istrue= true;
+
+//Console.WriteLine("Enter Your Name?");
+//string UserName= Console.ReadLine();
+
+//Console.WriteLine("Welcome, " + UserName);
+
+//Operators
+int a = 30, b = 10;
+//Arithmetical Operators
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a / b);
+Console.WriteLine(a % b);
+
+// assignment operators
+a += 23;// a = a + 23
+Console.WriteLine(a);
+a -= 23;// a = a - 23
+Console.WriteLine(a);
+a *= 3;// a = a * 3
+Console.WriteLine(a);
+a /= 3;// a = a / 3
+Console.WriteLine(a);
+
+
+// Comparison operaators
+Console.WriteLine(a == b);//False
+//Console.WriteLine(a != b);//True
+//Console.WriteLine(a < b);//False
+//Console.WriteLine(a > b);//True
+Console.WriteLine(a <= b);//False
+Console.WriteLine(a >= b);//True
+
+// Logical Operators
+//And
+
+Console.WriteLine(a <= b && a != b);
+
+//Or
+Console.WriteLine(a <= b || a != b);
+
+//Not
+Console.WriteLine(!(a <= b) && a != b);
+
+// Conditional Statements
+
+
+Console.WriteLine("Please Enter Your Age : ");
+
+int age = Int32.Parse(Console.ReadLine());
+
+//if else ladder
+if (age >18 )
+{
+    Console.WriteLine("You are Eligible to vote.");
+}
+else if (age == 18)
+{
+    //Nested if else ladder
+
+    Console.WriteLine("Do you have NIC..? Press 'Y' for yes and 'N' for no");
+
+    char status = char.Parse(Console.ReadLine());
+    if(status =='Y' || status == 'y')
+    {
+        Console.WriteLine("You can Vote.");
+    }
+    else if (status == 'N' || status == 'n')
+    {
+        Console.WriteLine("Sorry, You can't Vote.");
+    }
+   else 
+    {
+        Console.WriteLine("Press Y or N only.");
+    }
+}
+else
+{
+    Console.WriteLine("Sorry, You are not Eligible to vote.");
+}
+// Switch
+Console.WriteLine("Please enter number for days from 1 to 7");
+int day = Int32.Parse(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Today is Monday..");
+        break;
+    case 2:
+        Console.WriteLine("Today is Tuesday..");
+        break;
+    case 3:
+        Console.WriteLine("Today is Wednesday..");
+        break;
+    case 4:
+        Console.WriteLine("Today is Thursday..");
+        break;
+    case 5:
+        Console.WriteLine("Today is Friday..");
+        break;
+    case 6:
+        Console.WriteLine("Today is Saturday..");
+        break;
+    case 7:
+        Console.WriteLine("Today is Sunday..");
+        break;
+    default:
+        Console.WriteLine("Invalid Number");
+        break;
+
+}
+
+//Loops
+
+
+//For
+for (int i=1; i <= 100; i++)
+{
+    Console.WriteLine(i);
+}
+//reverse counting
+for (int i = 100; i <=1; i--)
+{
+    Console.WriteLine(i);
+}
+
+//even
+for (int i = 2; i <= 100; i += 2)
+{
+    Console.WriteLine(i);
+}
+//odd
+for (int i = 1; i <= 100; i += 2)
+{
+    Console.WriteLine(i);
+}
+//5 table
+for (int i = 5; i <= 50; i += 5)
+{
+    Console.WriteLine(i);
+}
+
+//While
+
+int j = 0;
+while ( j <= 10)
+{
+    Console.WriteLine(j);
+    j++;
+}
+
+*/
+//Do While
+
+int k = 0;
+do
+{
+    Console.WriteLine(k);
+    k++;
+
+}
+while (k > 10);
